69c7b4b1f1434c2d12bfdcc11721c14c1812292d
emad-pa <emadpa82@gmail.com>
2026-01-31 00:10:17 +0530

    env file updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV != "production") {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
